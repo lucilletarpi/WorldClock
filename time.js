645c40ae4468e33de0da353d517ff179149ae018
lucilletarpi <98407181+lucilletarpi@users.noreply.github.com>
2023-07-25 17:59:34 +0200

    start of css implementation of a loader and an api call condition

--- time.js
+++ time.js
@@ -1,23 +1,53 @@
 function time() {
+	const background = document.querySelector(".background");
+
+	const selectResult = document.getElementById("result");
 	const inputCountry = document.getElementById("country");
+
 	const apiKey = "41156f4f336b47b6851ed1226165cd23";
-	const country = inputCountry.value; // Remplacez par le pays
+	// Retrieves the country entered by the user
+	const country = inputCountry.value.toLowerCase();
+
+	// Erase old search
+	selectResult.innerHTML = "";
+
+	// Retrieve response
+	const parseResponse = (response) => response.json();
+
+	const loader = document.getElementById("loader");
+	loader.classList.remove("hide-loader");
 
 	fetch(
 		`https://timezone.abstractapi.com/v1/current_time/?api_key=${apiKey}&location=${country}`
 	)
-		.then((response) => response.json())
-		.then((data) => {
-			const localTime = new Date(data.datetime);
-			console.log(
-				`L'heure actuelle en ${country} est : ${localTime.toLocaleString()}`
-			);
+		.then((response) => {
+			if (!response.ok) {
+				throw new Error("Oops! Merci de réssayer");
+			} else return parseResponse(response);
 		})
-		.catch((error) =>
-			console.error(
-				"Une erreur s'est produite lors de la récupération des données :",
-				error
-			)
-		);
+		.then((data) => {
+			loader.classList.add("hide-loader");
+			if (country === data.requested_location) {
+				const localTime = new Date(data.datetime);
+				const showResult = document.createElement("p");
+				showResult.className = "timeResult";
+				showResult.id = `${country}`;
+				showResult.innerHTML = `${country} ${localTime.toLocaleString()}`;
+				selectResult.appendChild(showResult);
+				background.id = data.timezone_abbreviation;
+			} else {
+				const showResult = document.createElement("p");
+				showResult.className = "errorResult";
+				showResult.innerHTML = "Oops! On ne connait pas ce pays";
+				selectResult.appendChild(showResult);
+				background.id = "noCountry";
+			}
+		})
+		.catch(() => {
+			const showResult = document.createElement("p");
+			showResult.className = "errorResult";
+			showResult.innerHTML = "Oops! Merci de réssayer";
+			selectResult.appendChild(showResult);
+		});
 }
 time();
